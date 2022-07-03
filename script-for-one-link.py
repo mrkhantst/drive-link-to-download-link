1a9fe3ffdd05c86949e59f410f5b03f651f2e757
Si Thu Khant <sithukhant4768@gmail.com>
2022-07-03 16:52:10 +0630

    Updated Sun 3 Jul 2022

--- script-for-one-link.py
+++ script-for-one-link.py
@@ -1,7 +1,8 @@
+# First You need to import regex (Download it if you haven't yet)
 import regex
 
 link = input("Enter your drive link: ")
 
 download_link = link.replace("file/d/", "uc?id=").replace("/view?usp=sharing", "&export=download")
 
-print(download_link)
+print("Oop, here is your download link: ", download_link)
